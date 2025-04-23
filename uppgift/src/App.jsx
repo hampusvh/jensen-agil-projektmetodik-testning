2ecd0622e5ba2efd65bab6b7d810255afd6eb11b
hampusvh <hampus.vh@gmail.com>
2025-04-23 14:04:12 +0200

    backup code of work

--- uppgift/src/App.jsx
+++ uppgift/src/App.jsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
+import Login from "./components/Login/Login";
 import RegisterMovie from "./components/RegisterMovie";
 import ListMovies from "./components/ListMovies";
 
 function App() {
+  const [token, setToken] = useState("");
+
   return (
     <div>
-      <RegisterMovie />
-      <hr />
-      <ListMovies />
+      {!token && <Login onLogin={(jwt) => setToken(jwt)} />}
+
+      {token && (
+        <>
+          <RegisterMovie token={token} />
+          <hr />
+          <ListMovies token={token} />
+        </>
+      )}
     </div>
   );
 }
---
+++ uppgift/src/components/ListMovies/ListMovies.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import "../../App.css";
+
+function ListMovies({ token }) {
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const res = await fetch(
+          "https://tokenservice-jwt-2025.fly.dev/movies",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        if (res.ok) {
+          const data = await res.json();
+          setMovies(data);
+        } else {
+          const errMsg = await res.text();
+          setError("Kunde inte hämta filmer: " + errMsg);
+        }
+      } catch (err) {
+        setError("Något gick fel: " + err.message);
+      }
+    };
+
+    fetchMovies();
+  }, [token]);
+
+  return (
+    <div className="movies-container">
+      <h3>Aktuell lista av filmer</h3>
+      {error && <p className="error-message">{error}</p>}
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.id}>
+            <strong>{movie.title}</strong> ({movie.productionYear}) – ID:{" "}
+            {movie.id}
+            <br />
+            Regissör: {movie.director}
+            <br />
+            Beskrivning: {movie.description}
+            <hr />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ListMovies;
